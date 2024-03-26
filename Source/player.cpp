ad2fd056d2dd4070849930ee3632d13ea2a8d30e
KPhoenix <ericjrobinson@live.com>
2024-03-26 17:41:27 -0400

    Enforce play within restricted screen area

--- Source/player.cpp
+++ Source/player.cpp
@@ -3154,6 +3154,11 @@ void CheckPlrSpell(bool isShiftHeld, SpellID spellID, SpellType spellType)
 	assert(MyPlayer != nullptr);
 	Player &myPlayer = *MyPlayer;
 
+	if (GetSpellData(spellID).isTargeted()) {
+		if (!IsScreenPosLegalPlayArea(MousePosition.x, MousePosition.y))
+			return;
+	}
+
 	if (!IsValidSpell(spellID)) {
 		myPlayer.Say(HeroSpeech::IDontHaveASpellReady);
 		return;
