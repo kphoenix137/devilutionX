a32c819dbae15692b6390476f76f4e9c76349402
Eric Robinson <ericjrobinson@live.com>
2025-03-19 15:44:16 -0400

    Allow 1.09 Missing Logic in Hellfire

--- Source/player.cpp
+++ Source/player.cpp
@@ -206,13 +206,11 @@ void StartRangeAttack(Player &player, Direction d, WorldTileCoord cx, WorldTileC
 	int8_t skippedAnimationFrames = 0;
 	const auto flags = player._pIFlags;
 
-	if (!gbIsHellfire) {
-		if (includesFirstFrame && HasAnyOf(flags, ItemSpecialEffect::QuickAttack | ItemSpecialEffect::FastAttack)) {
-			skippedAnimationFrames += 1;
-		}
-		if (HasAnyOf(flags, ItemSpecialEffect::FastAttack)) {
-			skippedAnimationFrames += 1;
-		}
+	if (includesFirstFrame && HasAnyOf(flags, ItemSpecialEffect::QuickAttack | ItemSpecialEffect::FastAttack)) {
+		skippedAnimationFrames += 1;
+	}
+	if (HasAnyOf(flags, ItemSpecialEffect::FastAttack)) {
+		skippedAnimationFrames += 1;
 	}
 
 	auto animationFlags = AnimationDistributionFlags::ProcessAnimationPending;
