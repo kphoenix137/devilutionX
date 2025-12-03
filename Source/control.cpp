44ddeaf7377a4815250e7868d715633d92204c91
Eric Robinson <ericjrobinson@live.com>
2025-12-03 05:21:36 -0500

    Fix statting on death crash

--- Source/control.cpp
+++ Source/control.cpp
@@ -1631,6 +1631,9 @@ void CheckChrBtns()
 {
 	const Player &myPlayer = *MyPlayer;
 
+	if (myPlayer._pmode == PM_DEATH)
+		return;
+
 	if (CharPanelButtonActive || myPlayer._pStatPts == 0)
 		return;
 
@@ -1649,6 +1652,11 @@ void CheckChrBtns()
 
 void ReleaseChrBtns(bool addAllStatPoints)
 {
+	const Player &myPlayer = *MyPlayer;
+
+	if (myPlayer._pmode == PM_DEATH)
+		return;
+
 	CharPanelButtonActive = false;
 	for (auto attribute : enum_values<CharacterAttribute>()) {
 		auto buttonId = static_cast<size_t>(attribute);
