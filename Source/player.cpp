82fd26eb53d3ade9a7e4d2a5b5b8d8327f181586
Eric Robinson <ericjrobinson@live.com>
2025-12-10 16:25:37 -0500

    Update player.cpp

--- Source/player.cpp
+++ Source/player.cpp
@@ -2399,7 +2399,7 @@ void NextPlrLevel(Player &player)
 
 	if (&player == MyPlayer) {
 		RedrawComponent(PanelDrawComponent::Health);
-	}sfSDFSDF
+	}
 
 	const int mana = player.getClassAttributes().lvlMana;
 
